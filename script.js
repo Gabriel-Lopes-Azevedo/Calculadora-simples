8f130c43e56d5752b7abce2b1de7ebcce4aabf1c
Gabriel Lopes Azevedo <93387104+Gabriel-Lopes-Azevedo@users.noreply.github.com>
2022-06-27 16:56:56 -0300

    Upando calculadora

---
+++ script.js
@@ -0,0 +1,59 @@
+function somar () {
+    let n1 = document.getElementById("numero-1");
+    let n2 = document.getElementById("numero-2");
+    let n1Formatado = parseInt(n1.value);
+    let n2Formatado = parseInt(n2.value);
+    let res = document.getElementById("resultado");
+    let soma = n1Formatado + n2Formatado;
+
+    if (n1.value === "" || n2.value === "") {
+        alert("Campo vazio detectado, insira um número para realizar o cálculo!");
+    } else {
+        res.innerHTML = "O resultado da soma é " + soma + ".";       
+    }
+}
+
+function subtrair () {
+    let n1 = document.getElementById("numero-1");
+    let n2 = document.getElementById("numero-2");
+    let n1Formatado = parseInt(n1.value);
+    let n2Formatado = parseInt(n2.value);
+    let res = document.getElementById("resultado");
+    let sub = n1Formatado - n2Formatado;
+
+    if (n1.value === "" || n2.value === "") {
+        alert("Campo vazio detectado, insira um número para realizar o cálculo!");
+    } else {
+        res.innerHTML = "O resultado da soma é " + sub + ".";       
+    }
+}
+
+function dividir () {
+    let n1 = document.getElementById("numero-1");
+    let n2 = document.getElementById("numero-2");
+    let n1Formatado = parseInt(n1.value);
+    let n2Formatado = parseInt(n2.value);
+    let res = document.getElementById("resultado");
+    let div = n1Formatado / n2Formatado;
+
+    if (n1.value === "" || n2.value === "") {
+        alert("Campo vazio detectado, insira um número para realizar o cálculo!");
+    } else {
+        res.innerHTML = "O resultado da soma é " + div + ".";       
+    }
+}
+
+function multiplicar () {
+    let n1 = document.getElementById("numero-1");
+    let n2 = document.getElementById("numero-2");
+    let n1Formatado = parseInt(n1.value);
+    let n2Formatado = parseInt(n2.value);
+    let res = document.getElementById("resultado");
+    let mul = n1Formatado * n2Formatado;
+
+    if (n1.value === "" || n2.value === "") {
+        alert("Campo vazio detectado, insira um número para realizar o cálculo!");
+    } else {
+        res.innerHTML = "O resultado da soma é " + mul + ".";    
+    }
+}
